eb209544792fc31d97fcf8201670fd4c3f0d4dcb
RageLeague <34314733+RageLeague@users.noreply.github.com>
2020-08-22 22:19:02 -0600

    Add comments

--- content/util.lua
+++ content/util.lua
@@ -1,16 +1,21 @@
+-- Defines the advisors and their character alias.
 local ADVISOR_IDS = {
     -- Elon Musk personality
+    -- or maybe the personification of reddit, haven't decided yet.
     advisor_diplomacy = "ENDO",
     -- Ben Sharpiro personality
     advisor_manipulate = "PLOCKA",
     -- Donald Trump personality
     advisor_hostile = "RAKE",
 }
+-- Defines the home for the advisors
 local ADVISOR_HOME = {
     advisor_diplomacy = "DIPL_PRES_OFFICE",
     advisor_manipulate = "MANI_PRES_OFFICE",
     advisor_hostile = "HOST_PRES_OFFICE",
 }
+-- Add the three advisors as casts in a quest.
+-- qdef: the quest to add the advisors
 local function AddAdvisors(qdef)
     for id, val in pairs(ADVISOR_IDS) do
         qdef:AddCastByAlias{
@@ -27,6 +32,7 @@ local function AddAdvisors(qdef)
         }
     end
 end
+-- Add the home location to the quest. Typically done when you need to go home.
 local function AddHomeCasts(qdef)
     qdef:AddLocationCast{
         cast_id = "home",
@@ -51,6 +57,7 @@ local function AddHomeCasts(qdef)
     }
 end
 
+-- Add an optional primary advisor to the quest. might change the dialog of side quests.
 local function AddOptionalPrimaryAdvisor(qdef, not_optional)
     qdef:AddCast{
         cast_id = "primary_advisor",
@@ -71,6 +78,7 @@ local function AddOptionalPrimaryAdvisor(qdef, not_optional)
     }
 end
 
+-- Start the free time event. spawn the opportunity and change the actions
 local function StartFreeTime(actions)
     local quest = QuestUtil.SpawnQuest("FREE_TIME_EVENT")
     if quest and actions then
@@ -85,21 +93,31 @@ local SUPPORT_DELTA = {
     [RELATIONSHIP.LIKED] = 30,
     [RELATIONSHIP.LOVED] = 60,
 }
+
+-- Calculate the score for an agent's support. Used as score_fn in agent casts.
+-- Has a built-in randomizer. Generally speaking, an agent that likes you more, whose
+-- faction supports you more, and whose wealth class supports you more, will be more 
+-- likely to be casted.
 local function SupportScore(agent)
     return TheGame:GetGameState():GetMainQuest():DefFn("GetSupportForAgent", agent)
         + SUPPORT_DELTA[agent:GetRelationship()] + (math.random() * 30) -15
 end
 
+-- The opposite of SupportScore
 local function OppositionScore(agent)
     return -SupportScore(agent)
 end
 
+-- Check if an agent is a valid random bystander.
+-- A valid bystander should be non-retired, not in hiding, not in player party, is sentient, doesn't
+-- have plot armor, and isn't on duty.
 local function RandomBystanderCondition(agent)
     return not (AgentUtil.IsInHiding(agent) or agent:IsRetired() or agent:IsInPlayerParty()
         or AgentUtil.HasPlotArmour(agent) or not agent:IsSentient())
-        and not agent:GetBrain():IsOnDuty()
+        and not (agent:GetBrain() and agent:GetBrain():IsOnDuty())
 end
 
+-- Do the convo for unlocking a location.
 local function DoLocationUnlock(cxt, id)
     if not table.arraycontains(TheGame:GetGameState():GetMainQuest().param.unlocked_locations, id) then
         cxt:Opt("OPT_UNLOCK_NEW_LOCATION",TheGame:GetGameState():GetLocation(id))
@@ -112,7 +130,7 @@ local function DoLocationUnlock(cxt, id)
     end
 end
 
-
+-- Get wealth based on renown.
 local function GetWealth(renown)
     renown = renown or 1
     return clamp(renown, 1, DemocracyConstants.wealth_levels)
@@ -127,6 +145,7 @@ local function GetWealthColor(renown)
     return DemocracyConstants.wealth_color[GetWealth(renown)]
 end
 
+-- Add the cast for each opposition candidates, defined in opposition_candidates.lua
 local function AddOppositionCast(qdef)
     for _, data in pairs(DemocracyConstants.opposition_data) do
         if data.character and data.character ~= "" then
