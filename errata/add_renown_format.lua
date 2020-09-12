bc9698810aea1b65dbb9402628cb08b5b16ff83c
RageLeague <34314733+RageLeague@users.noreply.github.com>
2020-09-12 17:59:33 -0400

    policy consequences

--- errata/add_renown_format.lua
+++ errata/add_renown_format.lua
@@ -1,3 +1,10 @@
 loc.wealth_name = function(renown)
     return DemocracyUtil.GetWealthString(renown)
 end
+loc.wealth_name_list = function(list)
+    local t = {}
+    for i, renown in pairs( list ) do
+        table.insert( t, loc.format( "{1#wealth_name}", renown ))
+    end
+    return loc.format( "{1#listing}", t )
+end
