fc3a5da3330a4afaa351a1a025339fea3f5c2707
RageLeague <34314733+RageLeague@users.noreply.github.com>
2020-12-22 10:41:00 -0700

    Create race_debate_scrum.lua

---
+++ content/quests/main/race_debate_scrum.lua
@@ -0,0 +1,163 @@
+local QDEF = QuestDef.Define
+{
+    title = "Debate Scrum",
+    desc = "You are invited to the presidential debate with other candidates. Impress the audience with your debate skills.",
+    icon = engine.asset.Texture("DEMOCRATICRACE:assets/quests/interview.png"),
+
+    qtype = QTYPE.STORY,
+    collect_agent_locations = function(quest, t)
+        -- if quest:IsActive("return_to_advisor") then
+        --     table.insert(t, { agent = quest:GetCastMember("primary_advisor"), location = quest:GetCastMember('home')})
+        -- else
+        table.insert(t, { agent = quest:GetCastMember("primary_advisor"), location = quest:GetCastMember('backroom'), role = CHARACTER_ROLES.VISITOR})
+        -- end
+        table.insert(t, { agent = quest:GetCastMember("host"), location = quest:GetCastMember('theater')})
+        for id, data in pairs(DemocracyConstants.opposition_data) do 
+            table.insert(t, { agent = quest:GetCastMember(id), location = quest:GetCastMember('theater'), role = CHARACTER_ROLES.VISITOR})
+        end
+    end,
+    -- on_start = function(quest)
+
+    -- end,
+    -- on_complete = function(quest)
+    --     if quest:GetCastMember("primary_advisor") then
+    --         quest:GetCastMember("primary_advisor"):GetBrain():SendToWork()
+    --     end
+    -- end,
+    on_destroy = function(quest)
+        quest:GetCastMember("primary_advisor"):GetBrain():SendToWork()
+        if quest.param.parent_quest then
+            quest.param.parent_quest.param.did_debate_scrum = true
+        end
+    end,
+}
+:AddCast{
+    cast_id = "host",
+    cast_fn = function(quest, t) 
+        if quest:GetCastMember("theater"):GetProprietor() then
+            table.insert(t, quest:GetCastMember("theater"):GetProprietor())
+        end
+    end,
+    when = QWHEN.MANUAL,
+    events = 
+    {
+        agent_retired = function( quest, agent )
+            -- if quest:IsActive( "get_snail" ) then
+                -- If noodle chef died before we even got the snail, cast someone new.
+                quest:UnassignCastMember( "host" )
+                quest:AssignCastMember( "host" )
+            -- end
+        end,
+    },
+}
+:AddCastFallback{
+    cast_fn = function(quest, t)
+        quest:GetCastMember("theater"):GetWorkPosition("host"):TryHire()
+        if quest:GetCastMember("theater"):GetProprietor() then
+            table.insert(t, quest:GetCastMember("theater"):GetProprietor())
+        end
+    end,
+}
+:AddCast{
+    cast_id = "audience",
+    when = QWHEN.MANUAL,
+    condition = function(agent, quest)
+        return not table.arraycontains(quest.param.audience or {}, agent)
+    end,
+    score_fn = function(agent, quest)
+        if agent:HasAspect( "bribed" ) then
+            return 100
+        end
+        local sc = agent:GetRenown() * 2
+        if agent:GetRelationship() ~= RELATIONSHIP.NEUTRAL then
+            sc = sc + 5
+        end
+        return math.random(sc, 20)
+    end,
+    on_assign = function(quest, agent)
+        if not quest.param.audience then
+            quest.param.audience = {}
+        end
+        table.insert(quest.param.audience, agent)
+    end,
+}
+:AddCastFallback{
+    cast_fn = function(quest, t)
+        table.insert( t, quest:CreateSkinnedAgent() )
+    end,
+}
+:AddLocationCast{
+    cast_id = "theater",
+    cast_fn = function(quest, t)
+        table.insert(t, TheGame:GetGameState():GetLocation("GRAND_THEATER"))
+    end,
+    on_assign = function(quest, location)
+        -- quest:SpawnTempLocation("BACKROOM", "backroom")
+        quest:AssignCastMember("host")
+    end,
+    no_validation = true,
+}
+:AddLocationCast{
+    cast_id = "backroom",
+    no_validation = true,
+    cast_fn = function(quest, t)
+        table.insert(t, TheGame:GetGameState():GetLocation("GRAND_THEATER.backroom"))
+    end,
+    -- on_assign = function(quest, location)
+
+    --     -- print(location)
+    --     -- print(quest:GetCastMember("theater"))
+    --     -- print(quest:GetCastMember("theater"):GetMapPos())
+    --     -- location:SetMapPos( quest:GetCastMember("theater"):GetMapPos() )
+    -- end,
+    -- when = QWHEN.MANUAL,
+}
+:AddObjective{
+    id = "go_to_debate",
+    title = "Go to the debate",
+    desc = "Meet up with {primary_advisor} at the Grand Theater.",
+    mark = {"backroom"},
+    state = QSTATUS.ACTIVE,
+}
+:AddObjective{
+    id = "do_debate",
+    title = "Do the debate",
+    desc = "Try to stand out, but not in a bad way.",
+    mark = {"theater"},
+    -- state = QSTATUS.ACTIVE,
+}
+
+DemocracyUtil.AddPrimaryAdvisor(QDEF, true)
+DemocracyUtil.AddHomeCasts(QDEF)
+DemocracyUtil.AddOppositionCast(QDEF)
+
+QDEF:AddConvo("go_to_interview")
+    :ConfrontState("STATE_CONFRONT", function(cxt) return cxt.location == cxt.quest:GetCastMember("backroom") end)
+        :Loc{
+            DIALOG_INTRO = [[
+                * [p] You arrive at the grand theater backroom, where {agent} awaits you.
+                player:
+                    !left
+                agent:
+                    !right
+                    Are you ready for tonight's debate?
+                player:
+                    Are we ever ready?
+                agent:
+                    Good point.
+                    Anyway, whether you're ready or not, time to go.
+                    Good luck.
+            ]],
+        }
+        :Fn(function(cxt)
+            cxt:TalkTo(cxt:GetCastMember("primary_advisor"))
+            DemocracyUtil.PopulateTheater(cxt.quest, cxt.quest:GetCastMember("theater"), 8)
+            cxt:Dialog("DIALOG_INTRO")
+            cxt.quest:Complete("go_to_debate")
+            cxt.quest:Activate("do_debate")
+            cxt:Opt("OPT_LEAVE_LOCATION")
+                :Fn(function(cxt)
+                    cxt.encounter:DoLocationTransition(cxt.quest:GetCastMember("theater"))
+                end)
+                :MakeUnder()
+        end)
