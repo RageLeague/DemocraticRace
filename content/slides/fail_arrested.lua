fc52c9f46fdaa9dfd70c63743f2fa8a125a3ccc5
WumpusThe19th <70040747+WumpusThe19th@users.noreply.github.com>
2021-03-08 12:33:47 -0500

    Update fail_arrested.lua

--- content/slides/fail_arrested.lua
+++ content/slides/fail_arrested.lua
@@ -8,20 +8,21 @@ return
     {
         {
             mov = 'movies/smith_act1_slide_04.ogv',
-            txt = "You are arrested and taken into Admiralty custody. You will go under a criminal investigation.",
+            txt = "You were put into admiralty custody. While you technically under investigation, in the admiralty it's never that simple.",
         },
         {
             img = 'DEMOCRATICRACE:assets/slides/arrested_1.png',
-            txt = "However, due to the crippling bureaucracy present in the Admiralty, the investigation, "..
-            "which is supposed to be done within one or two days, has taken at least a week.\n\n" ..
-            "During this time, you are detained in a holding cell, with no way out, and certainly no way to run for a campaign.\n\n" ..
-            "Your advisor, when you needed them the most, is nowhere to be seen.",
+            txt = "The investigation was quick, or at least it should've been. However, due to your political and past affiliations.\n\n" ..
+            "You were kept for much longer as the higher ups tacked item after item onto the list of reasons to keep you locked up.\n\n" ..
+            "During this time, you were held in a jail cell, unable to go around and promote your platform as you had done before.\n\n" ..
+            "Even after you left, the scandal tainted your image for the rest of your political career.",
         },
         {
             mov = 'movies/rook_act4_slide_03c.ogv',
-            txt = "By the time you got out, it is too late.\n\n" ..
-            "The election is already over, and another candidate becomes the ruler of Havaria.\n\n" ..
-            "This is where your journey ends.",
+            txt = "Not to say there's much for a political career outside of the cell.\n\n" ..
+            "The election had ended by the time you left the jail, and some other big name became the president of Havaria.\n\n" ..
+            "Retired from your campaign, you silently slink back into your life as a roving grifter, never belonging, or trying to belong.\n\n" ..
+            "This is where your journey as a politician ends.",
         },
        
     }
