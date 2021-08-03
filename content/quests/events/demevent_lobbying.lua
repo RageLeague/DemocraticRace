3a3053d478ec901f9e4ac14d6b70cf9befea4662
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-08-02 21:29:03 -0700

    Update demevent_lobbying.lua

--- content/quests/events/demevent_lobbying.lua
+++ content/quests/events/demevent_lobbying.lua
@@ -66,6 +66,8 @@ QDEF:AddConvo()
         :SetLooping(true)
         :Fn(function(cxt)
             if cxt:FirstLoop() then
+                cxt.quest:Complete()
+
                 cxt.quest.param.lobby_money = 100
                 local weightings = {}
                 for id, data in pairs(DemocracyConstants.issue_data) do
@@ -74,5 +76,59 @@ QDEF:AddConvo()
                 local chosen = weightedpick(weightings)
                 cxt.quest.param.chosen_issue = chosen
                 cxt.quest.param.chosen_stance = DemocracyConstants.issue_data[chosen]:GetAgentStanceIndex(cxt:GetCastMember("merchant"))
+
+                cxt:Dialog("DIALOG_INTRO")
             end
+
+            local haggle_count = cxt.player.graft_owner:CountGraftsByID( "haggle_badge" )
+
+            if haggle_count > 0 and not cxt.quest.param.haggled_money then
+                local won_bonuses = {10}
+                cxt:BasicNegotiation("ASK_FOR_MORE", {
+                    on_start_negotiation = function(minigame)
+
+                        local amounts = {50, 20}
+
+                        for i = 2, haggle_count do
+                            table.insert(amount, 60)
+                        end
+
+                        for k,amt in ipairs(amounts) do
+                            local mod = minigame.opponent_negotiator:CreateModifier( "bonus_payment", amt )
+                            mod.result_table = won_bonuses
+                        end
+                    end,
+                    reason_fn = function(minigame)
+                        local total_amt = 0
+                        for k,v in pairs(won_bonuses) do
+                            total_amt = total_amt + v
+                        end
+                        return loc.format(cxt:GetLocString("NEGOTIATION_REASON"), total_amt )
+                    end,
+
+                    enemy_resolve_required = 10 * cxt.quest:GetRank(),
+                }):OnSuccess()
+                    :Fn(function(cxt)
+                        local total_bonus = 0
+                        for k,v in ipairs(won_bonuses) do
+                            total_bonus = total_bonus + v
+                        end
+                        cxt.quest.param.lobby_money = cxt.quest.param.lobby_money + total_bonus
+                        cxt.quest.param.haggled_money = true
+                    end)
+                    :OnFailure()
+                    :Fn(function(cxt)
+                        cxt.quest.param.haggled_money = true
+                    end)
+            end
+
+            cxt:Opt("OPT_ACCEPT")
+                :Dialog("DIALOG_ACCEPT")
+                :ReceiveMoney(cxt.quest.param.lobby_money)
+                :UpdatePoliticalStance(cxt.quest.param.chosen_issue, cxt.quest.param.chosen_stance, true)
+                :Travel()
+
+            cxt:Opt("OPT_DECLINE")
+                :Dialog("DIALOG_DECLINE")
+                :Travel()
         end)
