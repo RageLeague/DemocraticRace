11d0bd6cc7b42c9d80f40736a6c117f9cc7dc467
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-08-06 18:08:13 -0700

    Update the version numbers

--- modinit.lua
+++ modinit.lua
@@ -279,7 +279,7 @@ local MOD_OPTIONS =
 }
 -- print("Debug mode: " .. tostring(TheGame:GetLocalSettings().DEBUG))
 return {
-    version = "0.3.1",
+    version = "0.3.2",
     alias = "DEMOCRATICRACE",
 
     OnLoad = OnLoad,
