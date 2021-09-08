8d1b7b1c31124c26ac228ff2a56ec43b5e3c9af1
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-09-08 17:33:34 -0400

    Update calc_delta_modifier_event.lua

--- patches/general/calc_delta_modifier_event.lua
+++ patches/general/calc_delta_modifier_event.lua
@@ -5,8 +5,17 @@ end
 rawset(_G, patch_id, true)
 print("Loaded patch:"..patch_id)
 
+local negotiation_defs = require "negotiation/negotiation_defs"
+local EVENT = ExtendEnum(negotiation_defs.EVENT, {
+    "CALC_DELTA_MODIFIER",
+})
+
 local old_fn = Negotiation.Negotiator.DeltaModifier
 
 function Negotiation.Negotiator:DeltaModifier( modifier, delta, card )
-    old_fn(self, modifier, delta, card)
+    if not self.minigame.delta_modifier_accumulator then
+        self.minigame.delta_modifier_accumulator = CardEngine.ScalarAccumulator( self.minigame, EVENT.CALC_DELTA_MODIFIER )
+    end
+    local new_delta, details = self.minigame.delta_modifier_accumulator:CalculateValue( delta, self, modifier, card)
+    return old_fn(self, modifier, new_delta, card)
 end
