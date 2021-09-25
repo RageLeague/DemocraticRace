6d802ca5de2358a2a2da0992931ccc5a4ec91729
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-09-24 22:31:19 -0400

    Add a few shop defs

--- content/shop_defs.lua
+++ content/shop_defs.lua
@@ -4,7 +4,7 @@ local CARD_FLAGS = negotiation_defs.CARD_FLAGS
 -- Generate a shop that sells 6 negotiation cards. At least 3 common(one guaranteed to be of
 -- chosen type), 1 uncommon, and 1 uncommon or up of the chosen type
 local function GenerateCardShop(card_type, signature_id)
-    return function(stock) 
+    return function(stock)
         local NUM_STOCK = 6
         local cards = {}
         table.arrayadd( cards, NegotiationCardCollection.AllRewardableCards(
@@ -16,7 +16,7 @@ local function GenerateCardShop(card_type, signature_id)
         table.arrayadd( cards, NegotiationCardCollection.AllRewardableCards(
             function(cd) return CheckBits(cd.flags, card_type) and cd.rarity == CARD_RARITY.COMMON end
         ):Pick(1) )
-        
+
         local player_negotiator = TheGame:GetGameState():GetPlayerAgent().negotiator
         local has_signature = player_negotiator and player_negotiator:FindCard(function(card)
             return card.advisor == signature_id
@@ -24,7 +24,7 @@ local function GenerateCardShop(card_type, signature_id)
 
         if signature_id and math.random() < 0.5 then
             table.arrayadd( cards, NegotiationCardCollection(
-                function(cd) 
+                function(cd)
                     return cd.advisor == signature_id and not CheckBits(cd.flags, CARD_FLAGS.UPGRADED)
                 end
             ):Pick(1) )
@@ -46,9 +46,9 @@ local function GenerateCardShop(card_type, signature_id)
                 end
             ):Pick(remaining_num) )
         end
-    
+
         for i, card in ipairs(cards) do
-            AddCardToShop(stock, card.id)            
+            AddCardToShop(stock, card.id)
         end
     end
 end
@@ -59,5 +59,19 @@ CARD_SHOP_DEFS.RACE_HOSTILE_CARD_SHOP = GenerateCardShop(CARD_FLAGS.HOSTILE, "AD
 
 CARD_SHOP_DEFS.RISE_PROPAGANDA_SHOP = function(stock)
     -- there's not nearly enough items to make this shop work
-    AddShopItems(stock, 2, {"rise_manifesto", "rise_manifesto"})
+    AddShopItems(stock, 2, {"rise_manifesto", "rise_manifesto", "havarian_thesaurus"})
+end
+
+CARD_SHOP_DEFS.PARTY_SUPPLY_SHOP = function(stock)
+    -- A common item
+    AddShopItems(stock, 2, {"business_card", "hip_flask", "liquid_courage", "party_spirit", "gift_packaging", "pearl_grey"})
+    -- Uncommon or higher
+    AddShopItems(stock, 1, {"brain_gills", "pleasant_perfume", "mask_of_anonymity", "mask_of_intimidation", "mesmerizing_charm", "royal_relic", "curious_curio"})
+end
+
+CARD_SHOP_DEFS.BARON_HQ_SHOP_DEMOCRACY = function(stock)
+    -- A report
+    AddShopItems(stock, 2, {"business_card", "business_card", "spark_baron_permit", "spark_baron_permit", "work_report", "work_report", "research_report", "research_report", "executive_report"})
+    -- Weapon...?
+    AddShopItems(stock, 1, {"intimidating_blaster", "sequencer_negotiation", "paperweight", "robodex", "nano_lattice", "oshnu_glue", "targeting_core"})
 end
