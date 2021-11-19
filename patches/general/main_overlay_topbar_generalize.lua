22e210a7775aad168a2b720ce42483b06ef2d264
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-11-19 16:31:18 -0500

    Update main_overlay_topbar_generalize.lua

--- patches/general/main_overlay_topbar_generalize.lua
+++ patches/general/main_overlay_topbar_generalize.lua
@@ -5,6 +5,12 @@ end
 rawset(_G, patch_id, true)
 print("Loaded patch:"..patch_id)
 
+local NAV_SCREENS = { Screen.FeedbackScreen, Screen.NegotiationDeck, Screen.BattleDeck, Screen.GraftCollectionScreen, Screen.CoinScreen, Screen.BlasterScreen, Screen.MutatorsScreen, Screen.RelationshipScreen, Screen.BoonsBanesScreen, Screen.PauseMenu, Screen.PerkScreen }
+
+for i, class in ipairs(NAV_SCREENS) do
+    class.IS_NAV_SCREEN = true
+end
+
 local MainOverlayTopBar = Widget.MainOverlayTopBar
 
 local old_navigate_to = MainOverlayTopBar.NavigateTo
@@ -13,6 +19,9 @@ function MainOverlayTopBar:NavigateTo(screen, ...)
     local top_screen = old_navigate_to(self, screen, ...)
     -- This happens if the old screen is already processed, so we don't do extra things here
     if old_top_screen ~= top_screen then
+        if old_top_screen._class.IS_NAV_SCREEN and old_top_screen:IsOnStack() then
+            old_top_screen:FadeOut()
+        end
         return top_screen
     end
     if top_screen._class ~= screen._class and screen._class then
