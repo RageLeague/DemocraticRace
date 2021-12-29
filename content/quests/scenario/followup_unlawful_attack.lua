f8c0c7563ab5aca89d37e90fe7710ce10d940a16
RageLeague <34314733+RageLeague@users.noreply.github.com>
2021-12-29 12:38:30 -0700

    Update followup_unlawful_attack.lua

--- content/quests/scenario/followup_unlawful_attack.lua
+++ content/quests/scenario/followup_unlawful_attack.lua
@@ -11,6 +11,9 @@ local QDEF = QuestDef.Define
     events = {
         agent_retired = function(quest, agent)
             quest:Fail()
+            if quest.param.hire_amt then
+                TheGame:GetGameState():GetCaravan():AddMoney(quest.param.hire_amt)
+            end
         end,
     },
 }
@@ -21,6 +24,9 @@ local QDEF = QuestDef.Define
     events = {
         agent_retired = function(quest, agent)
             quest:Complete()
+            if quest.param.hire_amt then
+                TheGame:GetGameState():GetCaravan():AddMoney(quest.param.hire_amt)
+            end
         end,
     },
 }
@@ -29,9 +35,78 @@ local QDEF = QuestDef.Define
     id = "report",
     hide_in_overlay = true,
     on_activate = function(quest)
-        quest.param.negotiation_grafts = GenerateGrafts(GRAFT_TYPE.NEGOTIATION)
-        quest.param.combat_grafts = GenerateGrafts(GRAFT_TYPE.COMBAT)
-        quest:GetCastMember("plocka"):MoveToLocation(quest:GetCastMember("home"))
-        quest:GetCastMember("plocka"):SetLocationRole(CHARACTER_ROLES.VENDOR)
+        quest.param.attack_successful = DemocracyUtil.RandomGauss(quest.param.attack_difficulty or 0, 3) > 0
+
+        if quest.param.attack_successful then
+            if quest.param.no_kill then
+                quest.param.target_killed = false
+            elseif quest.param.must_kill then
+                quest.param.target_killed = true
+            else
+                quest.param.target_killed = math.random() < 0.5
+            end
+        end
     end,
 }
+
+QDEF:AddConvo("report")
+    :TravelConfront("STATE_CONF")
+        :Loc{
+            DIALOG_INTRO = [[
+                {not attack_successful?
+                    * [p] You saw {hunter}, who doesn't look so good.
+                    player:
+                        !left
+                    hunter:
+                        !right
+                        !injured
+                        Mission failed. {target} is tougher than I thought.
+                        I barely got away.
+                    player:
+                        Dang. I need to hire better people for this job.
+                    hunter:
+                        Anyway, I cannot rightfully take your money.
+                        Here is your money back.
+                }
+                {attack_successful?
+                    * [p] You saw {hunter}.
+                    player:
+                        !left
+                    hunter:
+                        !right
+                    {target_killed?
+                        {target} is dead.
+                    player:
+                        Nice work!
+                    }
+                    {not target_killed?
+                        I beat {target} up.
+                        {target.HeShe} shouldn't bother you, at least for a while.
+                    player:
+                        Excellent work!
+                    }
+                }
+            ]],
+        }
+        :Fn(function(cxt)
+            cxt:Dialog("DIALOG_INTRO")
+
+            if cxt.quest.param.attack_successful then
+                if cxt.quest.param.target_killed then
+                    cxt:GetCastMember("target"):Kill()
+                else
+                    cxt:GetCastMember("target"):GainAspect("stripped_influence", 4)
+                    cxt:GetCastMember("target"):GainAspect("intimidated")
+                    cxt:GetCastMember("target").health:SetPercent(math.random(15, 25)*0.01)
+                end
+            else
+                cxt:GetCastMember("hunter"):GainAspect("stripped_influence", 2)
+                cxt:GetCastMember("hunter").health:SetPercent(math.random(15, 25)*0.01)
+                if cxt.quest.param.hire_amt then
+                    cxt.enc:GainMoney( cxt.quest.param.hire_amt )
+                end
+            end
+
+            cxt.quest:Complete()
+            StateGraphUtil.AddLeaveLocation(cxt)
+        end)
