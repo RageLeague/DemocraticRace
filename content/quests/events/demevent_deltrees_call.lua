f0ed5ebc5c84f133a4d0c0ce2c6c6519c41c878f
RageLeague <34314733+RageLeague@users.noreply.github.com>
2022-02-10 22:34:35 -0500

    Change dialog to another flavor

--- content/quests/events/demevent_deltrees_call.lua
+++ content/quests/events/demevent_deltrees_call.lua
@@ -1,3 +1,6 @@
+local GOOD_POSTERS = {"PROP_PO_SUPERFICIAL", "PROP_PO_MESSY"} --delto likes bad posters and dislikes good posters.
+local BAD_POSTERS = {"PROP_PO_INSPIRING", "PROP_PO_THOUGHT_PROVOKING", "PROP_PO_MEDIOCRE"}
+
 local QDEF = QuestDef.Define
 {
     qtype = QTYPE.EVENT,
@@ -9,16 +12,13 @@ local QDEF = QuestDef.Define
   alias = "DELTREAN_DIGNITARY",
   no_validation = true,
 }
-
-local GOOD_POSTERS = {"PROP_PO_SUPERFICIAL", "PROP_PO_MESSY"} --delto likes bad posters and dislikes good posters.
-local BAD_POSTERS = {"PROP_PO_INSPIRING", "PROP_PO_THOUGHT_PROVOKING", "PROP_PO_MEDIOCRE"}
 :AddOpinionEvents{
     democracy_is_funny_joke =
     {
         delta = OPINION_DELTAS.LIKE,
         txt = "Convinced them Havarian Democracy is a ruse.",
     },
-	ugh_democracy = 
+	ugh_democracy =
 	{
 		delta = OPINION_DELTAS.DISLIKE,
 		txt = "Convinced them Havarian Democracy is the end of the status quo.",
@@ -29,72 +29,94 @@ QDEF:AddConvo()
     :ConfrontState("CONFRONT", function() return true end)
 		:Loc{
 			DIALOG_INTRO_DELTREAN = [[
+                * You are minding your own business when you hear a commotion.
+                * You see everyone's second favorite dignitary, yelling at {delto.hisher} companion in the public.
 				delto:
 					!right
-					!angry
-				player:
-					!left
-				delto:
+					!angry_shrug
 					Unbelievable. Unbelievable!
-					!angrypalm
-					The Havarians decide to create a democracy the moment I come on vacation.
-				player:
-					!crossed
-					Now what's wrong with Democracy, then?
-				delto:
-					What's wrong? Half of you people can't even read!
-					You're all straining the relations between Deltree and Havaria, and that makes my job harder!
+					!angry_accuse
+                    Who do these Havarians think they are? Thinking they can democratically elect a leader?
+                    Do they think Deltree is a joke? Making political decisions without our input?
+                * This does not like something you would like to be involved in, but {delto} doesn't seem to care about your opinion on this matter.
+                * {delto.HeShe} confronts you.
+                player:
+                    !left
+                delto:
+                    !angry_accuse
+                    You there!
+                player:
+                    !surprised
+                    What? Me?
+                delto:
+                    You look like one of those politicians running for presidency.
+                    Who do you think you are? Rebelling against us Deltrean?
+                    There is no order in this place!
+                * An angry diplomat on foreign soil is never a good thing.
+                * One wrong move, and the conflict can escalate into a war.
 				]],
-				
-			OPT_NEGOTIATE = "[p] Negotiate with {agent}.",
+
+			OPT_NEGOTIATE = "Placate {agent}",
 			DIALOG_NEGOTIATE_DELTO = [[
 				player:
-					!nudgenudge
-					Well who said it was a <i>real</> democracy?
+					!placate
+                    Hey, calm down. We don't seek independence.
 				]],
-				
-	--[[You tell Delto-dude that the democracy is a ruse. He's relieved.]]
-				
+
 			DIALOG_NEGOTIATE_DELTO_SUCCESS = [[
 				player:
-					Well of course it's all a ruse.
-					Most of the people running are just looking for power all the same.
-					Just look at me, one of the politicians. I know this better than anyone!
-				agent:
-					!angry
-					...
-					!happy
-					So just like the mainland, I see?
-					For a second, I thought you Havarians could get over your petty differences and be civil.
-					!chuckle
-					Glad I was wrong, though.
-					!give
-					Say...keep Havaria dependent on Deltree, and you'll see a lot more of this in the future.
-				player:
-					!take
-					Why of course. The Democracy is safe in my hands.
-				]],
-				
-	--[[You act too inspirational about democracy. He thinks it's a ploy to get out of deltrean rule]]
-				
+					Don't worry.
+                    !permit
+                    The election is simply an attempt to settle things peacefully, to end the endless conflicts between factions.
+                    !placate
+                    We aren't really seeking independence from Deltree.
+                    If anything, stabilizing Havaria through peaceful means will ensure that we serve Deltree better.
+                agent:
+                    !thought
+                    Hmm... This is unprecedented, and it's too early to tell if what you proposed work or not.
+                    Alright, we will give it a try.
+                    !throatcut
+                    If anything happens that we don't like, well...
+                player:
+                    Yeah, I get your point.
+                    I will do my best to ensure a favorable outcome for both Havaria and Deltree.
+                agent:
+                    !agree
+                    Very well. Glad to see some manners in Havaria.
+                * You might be seen as weak by the voters, but at least acting weak in front of a foreign dignitary is better than causing a war.
+                * You leave {agent} alone, hoping to never deal with {agent.himher} again.
+            ]],
+
 			DIALOG_NEGOTIATE_DELTO_FAILURE = [[
 				player:
-					Our Havarian democracy is built on the core priniciples of any good Deltrean colony.
-					!eureka
-					Yes, corruption and deciet runs rampant, but we also show all the values Deltree champions!
-					Freedom, Opportunity, and-
-				delto:
-					!palm
-					Don't tell me you idiots fell for those propaganda posters as well.
-					All you self-righteous idiots are going to cause a war, just you watch!
-				]],
-				
+                    !placate
+                    Look, we are simply choosing our leader via democracy.
+                    You shouldn't be concerned about it.
+                agent:
+                    !angry_accuse
+                    Who gives you the right to choose your own leader?
+                    Of course Deltree is going to be concerned! Havaria thinks it's an independent country and thinks it can choose its own leader!
+                player:
+                    !placate
+                    I wasn't-
+                agent:
+                    What other reason could you possibly have!
+                    !angry_shrug
+                    Unbelievable! These Havarians.
+                * You really could have handled this situation better.
+                * Now you have an angry Deltrean dignitary to deal with.
+                player:
+                    !thought
+                * Although you reasoned that {agent} is just constantly angry, and if Havaria hasn't been invaded by Deltree already, it's not going to now.
+                * You leave {agent} alone, hoping to never deal with {agent.himher} again.
+            ]],
+
 	--[[You show him a half-compotent poster. He gets genuinely worried about the fate of Havarian-Deltrean relations.]]
-	
+
 			OPT_SHOW_POSTER = "Show {agent} a poster",
 			DIALOG_BAD_POSTER = [[
 				player:
-					Now I'd say we're on track to keeping Havaria right under Deltree's thumb. 
+					Now I'd say we're on track to keeping Havaria right under Deltree's thumb.
 					!give
 					Just look at some of the material they're using to get elected.
 				delto:
@@ -105,11 +127,11 @@ QDEF:AddConvo()
 					!angry
 					It shouldn't be!
 					It's going to make people want to keep this democracy and not go back to Deltrean rule!
-					Unbelievable. You politicians are going to cause a war, just you wait. 
+					Unbelievable. You politicians are going to cause a war, just you wait.
 				]],
-			
+
 	--[[You show him a bad poster. He's reassured in his superiority complex over Havarians]]
-	
+
 			DIALOG_GOOD_POSTER = [[
 				player:
 					!chuckle
@@ -136,27 +158,56 @@ QDEF:AddConvo()
 					Right you are!
 					!exit
 				]],
-			
+
 			OPT_IGNORE = "Ignore {agent}.",
 			DIALOG_IGNORE_DELTREAN = [[
 				player:
 					!shrug
-					So? Deltree can either accept that we're free, or they can go shuck clams.
-					I'm just a politician, what can I say?
-				delto:
-					!angrypoint
-					You can say lots of things! Like how Havaria won't start a war with Deltree!
-				player:
-					!chuckle
-					What'd you take me for, an Oracle?
-				delto:
-					I took you for someone not willing to <i> Make my job a living Heshian Hell.</>
-				player:
-					You took me wrong, I guess. Now mosey on back to the mainland, hm?
-					!exit
-				delto:
-					!exit
-				]],
+                    I really have no say in this situation.
+                agent:
+                    You are a politician! You can say a lot of things!
+                player:
+                    Later!
+                    !exit
+                * You hastily leave the scene, not want to deal with this for any longer.
+                * As you leave, you hear {agent} continue to ramble.
+                agent:
+                    !crossed
+                    These Havarians are unbelievably rude.
+            ]],
+
+            OPT_INSULT = "Stand up for Havarian independence and insult {agent}",
+
+            DIALOG_INSULT = [[
+                player:
+                    !angry_shrug
+                    What if we are? What are you going to do about it?
+            ]],
+
+            DIALOG_INSULT_SUCCESS = [[
+                player:
+                    !angry_accuse
+                    You think just because you are a Deltrean, you are above us?
+                    You think you can just do whatever the Hesh you want like you own the place?
+                    !angry_shrug
+                    Well guess what? Havaria is an independent nation, and you should recognize it as such.
+                    You have no power here!
+                agent:
+                    This is an insult to the Deltrean Empire!
+                player:
+                    !crossed
+                    Wow, genius. How long did <i>you</> take to figure it out?
+                agent:
+                    !angry_accuse
+                    Know this, <i>Havarian</>. You will rue the day you insulted us.
+                    This will not go unpunished! Deltree will hear about this!
+                player:
+                    Fine! See if I care.
+                * You are 70% sure that being angry is just {agent}'s default state, that nothing serious will happen.
+                * Yet 30% of you feel like you have started something uncontrollable, like a wildfire.
+                * There is no taking back what you've just said now.
+                * At least at the moment it felt good, and the voters will see you as a hero standing up to Deltrean oppression.
+            ]],
 			}
             :Fn(function(cxt)
                 cxt.quest:Complete()
@@ -170,7 +221,7 @@ QDEF:AddConvo()
                 cxt:Dialog("DIALOG_INTRO_DELTREAN")
                 cxt:Opt("OPT_NEGOTIATE")
                     :Negotiation{
-                        on_success = function(cxt) 
+                        on_success = function(cxt)
                             cxt:Dialog("DIALOG_NEGOTIATE_DELTO_SUCCESS")
                             cxt.quest:OpinionEvent("delto", "democracy_is_funny_joke")
                             cxt.encounter:GainMoney( 100 )
