d61dc8416a700835991dac85b0073361a79b9bfb
RageLeague <34314733+RageLeague@users.noreply.github.com>
2022-02-20 19:37:24 -0500

    Update support threshold

--- content/quests/main/democratic_race_main.lua
+++ content/quests/main/democratic_race_main.lua
@@ -44,8 +44,8 @@ end
 
 local DAY_SCHEDULE = {
     {quest = "RACE_DAY_1", difficulty = 1, support_expectation = {0,10,25}},
-    {quest = "RACE_DAY_2", difficulty = 2, support_expectation = {25,40,55}},
-    {quest = "RACE_DAY_3", difficulty = 3, support_expectation = {55,75,95}},
+    {quest = "RACE_DAY_2", difficulty = 2, support_expectation = {35,50,65}},
+    {quest = "RACE_DAY_3", difficulty = 3, support_expectation = {80,100,120}},
     -- {quest = "RACE_DAY_4", difficulty = 4},
     -- {quest = "RACE_DAY_5", difficulty = 5},
 }
