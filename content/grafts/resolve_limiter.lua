6b85e250cbae77aaa933c87f31d3e382b6c1041a
RageLeague <34314733+RageLeague@users.noreply.github.com>
2022-04-28 22:15:12 -0600

    Update resolve_limiter.lua

--- content/grafts/resolve_limiter.lua
+++ content/grafts/resolve_limiter.lua
@@ -11,17 +11,51 @@ local GRAFTS =
     {
         hidden = true,
         desc = "Your max resolve in negotiation is limited by the proportion of health you have.",
+        negotiation_modifier =
+        {
+            hidden = true,
+            loc_strings =
+            {
+                DIALOG1 = [[
+                    player:
+                        !left
+                        !wince
+                        Ow! It hurts!
+                ]],
+                DIALOG2 = [[
+                    player:
+                        !left
+                        !wince
+                        Ah! The pain makes me unable to focus!
+                ]],
+                DIALOG3 = [[
+                    player:
+                        !left
+                        !wince
+                        This is not good!
+                ]],
+            },
+            event_handlers =
+            {
+                [ EVENT.BEGIN_NEGOTIATION ] = function( self, minigame )
+                    local core = self.negotiator:FindCoreArgument()
+                    local health_proportion = TheGame:GetGameState():GetPlayerAgent().health:GetPercent()
+                    assert(core, "Core not found")
+                    if core and health_proportion < 1 then
+                        self.engine:BroadcastEvent( EVENT.CUSTOM, self.OnQuipHurt, self )
+                        core.max_resolve = math.max(1, math.floor(core.max_resolve * health_proportion))
+                        core.resolve = math.min(core.resolve, core.max_resolve)
+                        core:ModifyResolve(0)
+                    end
+                end,
+            },
 
-        OnStartNegotiation = function( self, minigame )
-            local core = minigame:GetPlayerNegotiator():FindCoreArgument()
-            local health_proportion = TheGame:GetGameState():GetPlayerAgent().health:GetPercent()
-            assert(core, "Core not found")
-            if core then
-                core.max_resolve = math.max(1, math.floor(core.max_resolve * health_proportion))
-                core.resolve = math.min(core.resolve, core.max_resolve)
-                core:ModifyResolve(0)
-            end
-        end,
+            OnQuipHurt = function( panel, self )
+                local dialogid = table.arraypick{ "DIALOG1", "DIALOG2", "DIALOG3" }
+                local txt = loc.format( self.def:GetLocalizedString( dialogid ), self.enemy )
+                panel:Dialog( txt )
+            end,
+        },
     },
 }
 
