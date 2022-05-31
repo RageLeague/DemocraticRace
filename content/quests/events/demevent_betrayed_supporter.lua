e76fc2de7c261d71855713d0d07f68ebad4c0b5d
Cody Foehn <30597829+codyfun123@users.noreply.github.com>
2022-05-31 15:02:43 -0400

    Added encounter for a supporter with a conflicting stance

---
+++ content/quests/events/demevent_betrayed_supporter.lua
@@ -0,0 +1,122 @@
+local QDEF = QuestDef.Define
+{
+    qtype = QTYPE.EVENT,
+    act_filter = DemocracyUtil.DemocracyActFilter,
+    spawn_event_mask = QEVENT_TRIGGER.TRAVEL,
+}
+:AddCast{
+    cast_id = "supporter",
+    condition = function(agent, quest)
+        if agent:GetRelationship() >= RELATIONSHIP.NEUTRAL and DemocracyUtil.GetAgentEndorsement(agent) > RELATIONSHIP.NEUTRAL then
+            for id, data in pairs(DemocracyConstants.issue_data) do
+                local stance = data:GetAgentStanceIndex(agent)
+                local player_stance = DemocracyUtil.TryMainQuestFn("GetStance", id) or 0
+                if stance * player_stance < 0 then --looking for opposing stances by multiplying into a negative. two same signs or any zeroes will make this false
+                    print(agent.name, id, stance, player_stance)
+                    return true
+                end
+            end
+        end
+        return false
+    end,
+}
+
+QDEF:AddConvo()
+    :ConfrontState("STATE_CONFRONT")
+        :Loc{
+            DIALOG_INTRO = [[
+                * [p] You come across a concerned supporter of yours.
+                player:
+                    !left
+                supporter:
+                    !right
+                    !angry_accuse
+                    {player}! What in Hesh's name?!
+                player:
+                    What has you so riled up?
+                supporter:
+                    I supported you in the belief that you'd make the right decisions for Havaria, but I've just heard you argue for {bad_stance#pol_stance}? Unbelievable!
+            ]],
+            OPT_CHANGE = "Change to {good_stance#pol_stance} to appease {supporter}",
+            DIALOG_CHANGE = [[
+                player:
+                    [p] You know what? You're right. {good_stance#pol_stance} is what is right for Havaria, and that is what I will campaign for from here on out.
+                * Assuming you don't just flip-flop on this issue again.
+                supporter:
+                    !happy
+                    I knew I could count on you!
+            ]],
+            OPT_DENY = "Insist on {bad_stance#pol_stance}",
+            DIALOG_DENY = [[
+                player:
+                    !hips
+                    [p] {bad_stance#pol_stance} is the right choice for Havaria, like it or not.
+                supporter:
+                    !disappoint
+                    Is that so? Then maybe {player} is not the right choice for me. Goodbye.
+                    !exit
+            ]],
+            OPT_CONVINCE = "Convince {supporter} to look past this issue",
+            DIALOG_CONVINCE = [[
+                player:
+                    Now, just hear me out.
+            ]],
+            DIALOG_CONVINCE_SUCCESS = [[
+                player:
+                    !overthere
+                    [p] And that's why, with {bad_stance#pol_stance}, Havaria will be a better place overall.
+                supporter:
+                    !suspicious
+                    I'm still not entirely convinced that {bad_stance#pol_stance} is good, but you seem to know what you're doing. I trust you to make it work.
+            ]],
+            DIALOG_CONVINCE_FAILURE = [[
+                player:
+                    [p] See? A vote for me is a vote for a better Havaria!
+                supporter:
+                    What a waste of time. Goodbye, {player}.
+                    !exit
+            ]],
+            SIT_MOD_BAD = "{supporter} is not a fan of {bad_stance#pol_stance}",
+            SIT_MOD_VERY_BAD = "{supporter} loathes {bad_stance#pol_stance}",
+        }
+        :Fn(function(cxt)
+            cxt.quest:Complete()
+            cxt:TalkTo(cxt:GetCastMember("supporter"))
+            local supporter = cxt:GetCastMember("supporter")
+            local available_issues = DemocracyUtil.CollectIssueImportance(supporter)
+            local issue_id, stance, player_stance
+            while not issue_id do
+                assert_warning(#copykeys(available_issues) > 0, "Agent had no valid issues to disagree on")
+                issue_id = weightedpick(available_issues)
+                available_issues[issue_id] = nil
+                stance = DemocracyConstants.issue_data[issue_id]:GetAgentStanceIndex(supporter)
+                player_stance = DemocracyUtil.TryMainQuestFn("GetStance", issue_id) or 0
+                if stance * player_stance >= 0 then --again, only continue with opposing stances
+                    issue_id = nil
+                end
+            end
+            local issue = DemocracyConstants.issue_data[issue_id]
+            cxt.quest.param.good_stance = issue:GetStance(stance)
+            cxt.quest.param.bad_stance = issue:GetStance(player_stance)
+            local sit_mod = { value = 10, text = cxt:GetLocString("SIT_MOD_BAD") }
+            if (math.min(math.abs(stance), math.abs(player_stance)) == 2) then --extra hard if and only if you are both extremists in your opposed beliefs
+                sit_mod = { value = 20, text = cxt:GetLocString("SIT_MOD_VERY_BAD") }
+            end
+            cxt:Dialog("DIALOG_INTRO")
+            cxt:Opt("OPT_CHANGE")
+                :Dialog("DIALOG_CHANGE")
+                :UpdatePoliticalStance(issue, stance)
+                :Travel()
+            cxt:Opt("OPT_DENY")
+                :Dialog("DIALOG_DENY")
+                :ReceiveOpinion(OPINION.DISLIKE_IDEOLOGY)
+                :Travel()
+            cxt:BasicNegotiation("CONVINCE", {
+                situation_modifiers = {sit_mod},
+            })
+                :OnSuccess()
+                    :Travel()
+                :OnFailure()
+                    :ReceiveOpinion(OPINION.DISLIKE_IDEOLOGY)
+                    :Travel()
+        end)
