e09be8da7e4f626aec779bc7df178eee9aaf849e
RageLeague <34314733+RageLeague@users.noreply.github.com>
2023-01-01 20:17:57 -0700

    Update advisor_signature_cards.lua

--- content/negotiation/advisor_signature_cards.lua
+++ content/negotiation/advisor_signature_cards.lua
@@ -376,83 +376,137 @@ local CARDS = {
         name = "Initial Straw Army",
         flags = CARD_FLAGS.MANIPULATE | CARD_FLAGS.AMBUSH,
     },
-    advisor_manipulate_gaslighting =
+    -- advisor_manipulate_gaslighting =
+    -- {
+    --     name = "Gaslighting",
+    --     desc = "Remove target argument or bounty you control. Until the beginning of your next turn, "
+    --         .. "all opponent sources that are targeting it deal no damage.",
+    --     flavour = "'I have no idea what you're talking about. You must be crazy.'",
+    --     icon = "DEMOCRATICRACE:assets/cards/gaslighting.png",
+
+    --     advisor = "ADVISOR_MANIPULATE",
+    --     flags = CARD_FLAGS.MANIPULATE,
+    --     cost = 1,
+
+    --     target_self = TARGET_FLAG.ARGUMENT | TARGET_FLAG.BOUNTY,
+    --     modifier =
+    --     {
+    --         hidden = true,
+
+    --         event_priorities =
+    --         {
+    --             [ EVENT.CALC_PERSUASION ] = EVENT_PRIORITY_CLAMP + 100,
+    --         },
+
+    --         event_handlers =
+    --         {
+    --             [ EVENT.CALC_PERSUASION ] = function( self, source, persuasion )
+    --                 if self.affected_sources and table.arraycontains(self.affected_sources, source) then
+    --                     persuasion:ModifyPersuasion(0, 0, self)
+    --                 end
+    --             end,
+
+    --             [ EVENT.BEGIN_TURN ] = function( self, minigame, negotiator )
+    --                 if negotiator == self.negotiator then
+    --                     self.negotiator:RemoveModifier( self )
+    --                 end
+    --             end,
+    --         }
+    --     },
+    --     OnPostResolve = function( self, minigame, targets )
+    --         for i, target in ipairs(targets) do
+    --             local sources = {}
+    --             if minigame:IsBeingTargettedBy( target, self.anti_negotiator, sources ) then
+    --                 local mod = self.negotiator:CreateModifier( "advisor_manipulate_gaslighting", 1, self )
+    --                 mod.affected_sources = sources
+    --             end
+    --             target.negotiator:RemoveModifier( target )
+    --             if self.PostProcess then
+    --                 self:PostProcess(target, sources)
+    --             end
+    --         end
+    --     end,
+    -- },
+
+    -- advisor_manipulate_gaslighting_plus =
+    -- {
+    --     name = "Doubtful Gaslighting",
+    --     alt_desc = "<#UPGRADE>{INCEPT} {DOUBT} equal to the number of sources affected.</>",
+    --     desc_fn = function(self, fmt_str)
+    --         return fmt_str .. "\n" .. (self.def or self):GetLocalizedString("ALT_DESC")
+    --     end,
+    --     PostProcess = function(self, target, sources)
+    --         if sources and #sources > 0 then
+    --             self.anti_negotiator:InceptModifier("DOUBT", #sources , self )
+    --         end
+    --     end,
+    -- },
+    -- advisor_manipulate_gaslighting_plus2 =
+    -- {
+    --     name = "Visionary Gaslighting",
+    --     alt_desc = "<#UPGRADE>Draw cards equal to the number of sources affected.</>",
+    --     desc_fn = function(self, fmt_str)
+    --         return fmt_str .. "\n" .. (self.def or self):GetLocalizedString("ALT_DESC")
+    --     end,
+    --     PostProcess = function(self, target, sources)
+    --         if sources and #sources > 0 then
+    --             self.engine:DrawCards(#sources)
+    --         end
+    --     end,
+    -- },
+
+    advisor_manipulate_cognitive_dissonance =
     {
-        name = "Gaslighting",
-        desc = "Remove target argument or bounty you control. Until the beginning of your next turn, "
-            .. "all opponent sources that are targeting it deal no damage.",
-        flavour = "'I have no idea what you're talking about. You must be crazy.'",
+        name = "Cognitive Dissonance",
+        desc = "Remove target argument, bounty, or inception. Its controller creates a copy of it at the start of your next turn.",
+        flavour = "'When some people's belief contradicts the reality, instead of changing their belief, they reject reality.'",
         icon = "DEMOCRATICRACE:assets/cards/gaslighting.png",
 
         advisor = "ADVISOR_MANIPULATE",
         flags = CARD_FLAGS.MANIPULATE,
         cost = 1,
 
-        target_self = TARGET_FLAG.ARGUMENT | TARGET_FLAG.BOUNTY,
+        target_self = TARGET_FLAG.ARGUMENT | TARGET_FLAG.BOUNTY | TARGET_FLAG.INCEPTION,
+        target_enemy = TARGET_FLAG.ARGUMENT | TARGET_FLAG.BOUNTY | TARGET_FLAG.INCEPTION,
+
         modifier =
         {
             hidden = true,
 
-            event_priorities =
-            {
-                [ EVENT.CALC_PERSUASION ] = EVENT_PRIORITY_CLAMP + 100,
-            },
-
             event_handlers =
             {
-                [ EVENT.CALC_PERSUASION ] = function( self, source, persuasion )
-                    if self.affected_sources and table.arraycontains(self.affected_sources, source) then
-                        persuasion:ModifyPersuasion(0, 0, self)
+                [ EVENT.BEGIN_PLAYER_TURN ] = function ( self, minigame )
+                    if self.stored_mod then
+                        self.stored_mod:Duplicate(self.negotiator)
                     end
+                    self.negotiator:RemoveModifier(self)
                 end,
-
-                [ EVENT.BEGIN_TURN ] = function( self, minigame, negotiator )
-                    if negotiator == self.negotiator then
-                        self.negotiator:RemoveModifier( self )
-                    end
-                end,
-            }
+            },
         },
+
         OnPostResolve = function( self, minigame, targets )
             for i, target in ipairs(targets) do
-                local sources = {}
-                if minigame:IsBeingTargettedBy( target, self.anti_negotiator, sources ) then
-                    local mod = self.negotiator:CreateModifier( "advisor_manipulate_gaslighting", 1, self )
-                    mod.affected_sources = sources
-                end
+                local new_mod = target.negotiator:CreateModifier(self.id, 1, self)
+                new_mod.stored_mod = target:Clone()
                 target.negotiator:RemoveModifier( target )
-                if self.PostProcess then
-                    self:PostProcess(target, sources)
-                end
+            end
+            if self.card_draw and self.card_draw > 0 then
+                minigame:DrawCards( self.card_draw )
             end
         end,
     },
+    advisor_manipulate_cognitive_dissonance_plus =
+    {
+        name = "Pale Cognitive Dissonance",
 
-    advisor_manipulate_gaslighting_plus =
-    {
-        name = "Doubtful Gaslighting",
-        alt_desc = "<#UPGRADE>{INCEPT} {DOUBT} equal to the number of sources affected.</>",
-        desc_fn = function(self, fmt_str)
-            return fmt_str .. "\n" .. (self.def or self):GetLocalizedString("ALT_DESC")
-        end,
-        PostProcess = function(self, target, sources)
-            if sources and #sources > 0 then
-                self.anti_negotiator:InceptModifier("DOUBT", #sources , self )
-            end
-        end,
+        cost = 0,
     },
-    advisor_manipulate_gaslighting_plus2 =
+    advisor_manipulate_cognitive_dissonance_plus2 =
     {
-        name = "Visionary Gaslighting",
-        alt_desc = "<#UPGRADE>Draw cards equal to the number of sources affected.</>",
-        desc_fn = function(self, fmt_str)
-            return fmt_str .. "\n" .. (self.def or self):GetLocalizedString("ALT_DESC")
-        end,
-        PostProcess = function(self, target, sources)
-            if sources and #sources > 0 then
-                self.engine:DrawCards(#sources)
-            end
-        end,
+        name = "Visionary Cognitive Dissonance",
+        desc = "Remove target argument, bounty, or inception. Its controller creates a copy of it at the start of your next turn.\n<#UPGRADE>Draw a card.</>",
+
+        card_draw = 1,
     },
     advisor_manipulate_moreef_defense =
     {
