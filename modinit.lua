3aae9dd9582812d06b97462ee87ea7e9df97cff6
RageLeague <34314733+RageLeague@users.noreply.github.com>
2023-01-13 19:52:48 -0500

    Uptick version and add changelog

--- modinit.lua
+++ modinit.lua
@@ -371,7 +371,7 @@ local MOD_OPTIONS =
 }
 -- print("Debug mode: " .. tostring(TheGame:GetLocalSettings().DEBUG))
 return {
-    version = "0.8.1",
+    version = "0.9.0",
     alias = "DEMOCRATICRACE",
 
     OnLoad = OnLoad,
