46a8a64e99e75ed87a3ce54a69d229947bbd97dd
RageLeague <34314733+RageLeague@users.noreply.github.com>
2025-12-23 17:48:00 -0700

    More centrism

--- content/quests/events/demevent_starving_worker.lua
+++ content/quests/events/demevent_starving_worker.lua
@@ -316,6 +316,7 @@ QDEF:AddConvo()
 
             cxt:Opt("OPT_GIVE_A_LITTLE")
                 :Dialog("DIALOG_GIVE_A_LITTLE")
+                :UpdatePoliticalStance("FISCAL_POLICY", 0, false, true)
                 :DeliverMoney(SMALL_AMT, {no_scale = true})
                 :ReceiveOpinion(cxt.quest:GetQuestDef():GetOpinionEvent("gave_little"))
                 :Travel()
